bdb70b6f23f609e23a91643e094f02400bfac609
asdrubalsantander <asdrubalsantander@gmail.com>
2018-05-31 21:47:35 -0500

    Django Level Two project continue. TODO give style to the data

--- django_bootcamp_udemy/django_level_two_project/users/views.py
+++ django_bootcamp_udemy/django_level_two_project/users/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from users.models import Users
 # Create your views here.
 
 
 def users(request):
-    return render(request, 'users.html', context=None)
+    data = Users.objects.all().values()
+    user_data = {'users': data,
+                 'otherdata': 'this is other data'
+                 }
+    return render(request, 'users.html', context=user_data)
