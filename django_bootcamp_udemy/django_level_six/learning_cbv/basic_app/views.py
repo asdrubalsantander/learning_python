ffbf8f7f4499620e8bb73d2613eb351042b8668f
Asdrubal Santander <asdrubalsantander@gmail.com>
2018-06-20 19:41:18 -0500

    Exercise from Django level five - lecture 159

--- django_bootcamp_udemy/django_level_six/learning_cbv/basic_app/views.py
+++ django_bootcamp_udemy/django_level_six/learning_cbv/basic_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import View
-from django.http import HttpResponse
+from django.views.generic import View, TemplateView
 
 
-class index_view(View):
-    def get(self, request):
-        return HttpResponse('CLASS VIEW')
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injected'] = 'YES'
+        return context
