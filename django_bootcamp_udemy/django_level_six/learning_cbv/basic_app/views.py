e744421ca01423750085a5421828f9a816572713
Asdrubal Santander <asdrubalsantander@gmail.com>
2018-06-20 22:13:20 -0500

    Exercise from Django level five - lecture 161

--- django_bootcamp_udemy/django_level_six/learning_cbv/basic_app/views.py
+++ django_bootcamp_udemy/django_level_six/learning_cbv/basic_app/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView, DetailView
+from . import models
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injected'] = 'YES'
-        return context
+
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model = models.School
+
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'schools_detail'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
