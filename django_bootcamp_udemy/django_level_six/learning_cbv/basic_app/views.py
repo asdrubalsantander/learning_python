c55c3028a2931c2ab1ce18965ccaa9c3d1e90b8b
Asdrubal Santander <asdrubalsantander@gmail.com>
2018-06-21 21:39:51 -0500

    Exercise from Django level six - lecture 162

--- django_bootcamp_udemy/django_level_six/learning_cbv/basic_app/views.py
+++ django_bootcamp_udemy/django_level_six/learning_cbv/basic_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import View, TemplateView, ListView, DetailView, CreateView, DeleteView, UpdateView
 from . import models
 
 
@@ -16,3 +17,18 @@ class SchoolDetailView(DetailView):
     context_object_name = 'schools_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
+
+
+class SchoolCreateView(CreateView):
+    fields = ('name', 'principal', 'location')
+    model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy('basic_app:list')
