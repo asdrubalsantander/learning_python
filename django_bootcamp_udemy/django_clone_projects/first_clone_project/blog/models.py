0cff4e812d9a4db2223f35cc4e9975eec56b264f
Asdrubal Santander <asdrubalsantander@gmail.com>
2018-06-26 22:02:10 -0500

    Finished models - first clone project - lecture 163

--- django_bootcamp_udemy/django_clone_projects/first_clone_project/blog/models.py
+++ django_bootcamp_udemy/django_clone_projects/first_clone_project/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    text = models.TextField(max_length=1024)
+    is_published = models.BooleanField(default=False)
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+    user = models.ForeignKey(User, models.PROTECT)
+
+
+class Comment(models.Model):
+    username = models.CharField(max_length=50)
+    text = models.CharField(max_length=240)
+    date_created = models.DateField(auto_now_add=True)
+    is_approved = models.BooleanField(default=False)
+    post = models.ForeignKey(Post, models.CASCADE)
+
+    def __str__(self):
+        return self.text
