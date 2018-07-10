539078032f8225f467abaaaf0771fd7ae6b9cba6
Asdrubal Santander <asdrubalsantander@gmail.com>
2018-07-09 19:38:04 -0500

    Models donde - SCP

--- django_bootcamp_udemy/django_clone_projects/second_clone_project/archi/models.py
+++ django_bootcamp_udemy/django_clone_projects/second_clone_project/archi/models.py
@@ -1,2 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Topic(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    created_date = models.DateField(auto_now_add=True)
+    user = models.ManyToManyField(User, blank=True)
+
+
+class Post(models.Model):
+    created_date = models.DateField(auto_now_add=True)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
