c4cab50858f3999fb51a45f3971d53ed6722726c
Asdrubal Santander <asdrubalsantander@gmail.com>
2018-07-15 07:47:25 -0500

    Join Group - SCP

--- django_bootcamp_udemy/django_clone_projects/second_clone_project/archi/urls.py
+++ django_bootcamp_udemy/django_clone_projects/second_clone_project/archi/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index_view, signup_view, create_group_view, list_groups_view, detail_group_view
+from .views import index_view, signup_view, create_group_view, list_groups_view, detail_group_view, join_group_view
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('groups/', list_groups_view, name="list_group"),
     path('group/create/', create_group_view, name="create_group"),
     path('group/<pk>/', detail_group_view, name="detail_group"),
+    path('group/<pk>/join', join_group_view, name="join_group")
 ]
--- django_bootcamp_udemy/django_clone_projects/second_clone_project/archi/views.py
+++ django_bootcamp_udemy/django_clone_projects/second_clone_project/archi/views.py
@@ -4,6 +4,7 @@ from .forms import UserCreateForm, GroupCreateForm
 from .models import Topic
 from django.forms.models import model_to_dict
 from pprint import pprint
+from django.contrib.auth.decorators import login_required
 
 
 def index_view(request):
@@ -22,6 +23,7 @@ def signup_view(request):
     return render(request, template_name='signup.html', context={'form': form})
 
 
+@login_required
 def create_group_view(request):
     if request.method == "POST":
         form = GroupCreateForm(request.POST)
@@ -46,3 +48,17 @@ def detail_group_view(request, pk):
     if request.user.is_authenticated:
         user_is_member = True if request.user.topic_set.filter(pk=pk).exists() else False
     return render(request, 'group_detail.html', context={'group': group_detail, 'member': user_is_member})
+
+
+@login_required
+def join_group_view(request, pk):
+    user_is_member = False
+    if request.method == "POST":
+        group = Topic.objects.get(pk=pk)
+        if 'leave_group' in request.POST:
+            group.user.remove(request.user)
+        elif 'join_group' in request.POST:
+            group.user.add(request.user)
+            user_is_member = True
+
+    return render(request, 'group_detail.html', context={'group': group, 'member': user_is_member})
